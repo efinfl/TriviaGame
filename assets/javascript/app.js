5301a59bd3ebabc93ff05702cc21ab0e4803f3ef
EF <ef@EFs-MacBook-Pro.local>
2018-11-15 17:20:04 -0500

    added psuedo coding to js file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+// Global Variable
+
+// Start Game with a startGame function that's triggered when when Start Game button is clicked.
+    // First set of questions is displayed.
+    // Timer Starts
+    // Displays a set of Questions each with a set of answeres.
+    // Each answere has an input type of chackbox prepended to it
+
+// Selection is stored in a variable
+
+// If user completes all selections before time is up, a button is pressed to display answers and how many are correct.
+    // Numvber of correct answeres added to **points**
+
+// Else game is stopped if timer reaches zero.
+
+// User is alerted that time's up.
+
+//  Answers and how many are correct is displayed
+    // Numvber of correct answeres added to **points**
+
+// User has option to tray another set of questions by pressing a button.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
