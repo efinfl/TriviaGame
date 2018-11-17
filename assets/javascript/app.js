139ddf2421a6930fb0421a4940689b61cde8c4f8
EF <ef@EFs-MacBook-Pro.local>
2018-11-17 15:35:36 -0500

    Contains all changes done during tutor session, but nothing is working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     // Global Variable
     // Questions and Answers objects
 
-    var game = [
+    var questions = [
         {
             question: "How many lives does a cat have?",
             answerChoices: ["9", "As many as it wants", "1", "10"],
@@ -21,33 +21,73 @@ $(document).ready(function () {
             correct: "Alan 'Ollie' Gelfand"
         }
 
-    ]
-    // Selected Answer
-    var userPick;
+    ];
 
-    // Correctly Answered Total
-    var totalCorrect = 0
+    var timer;
 
-    // Incorrect Total
-    var totalIncorrect = 0
+    var game = {
 
-    // Total number of quetions
-    var numberOfQuestionsAsked = 0
+        // Correctly Answered Total
+        totalCorrect: 0,
 
-    // Index variable
-    var i = 0;
+        // Incorrect Total
+        totalIncorrect: 0,
 
-    // Game sarts when start button is pressed.
-   
-    // Displays questions from object array
-    function nextQuestion() {
-        $("#dislplayQuestion").text(game[i].question);
-        $("#answer_1").text(game[i].answerChoices[0]);
-        $("#answer_2").text(game[i].answerChoices[1]);
-        $("#answer_3").text(game[i].answerChoices[2]);
-        $("#answer_4").text(game[i].answerChoices[3]);
-        console.log(game[i].answerChoices);
+        // Total number of quetions
+        // numberOfQuestionsAsked: totalCorrect + totalIncorrect,
+
+        // Index variable
+        i: 0,
+
+        timeAloted: 120,
+        
+        
+        
+        // Game sarts when start button is pressed.
+
+        start: function () {
+            timer = setInterval(game.countdown, 1000);
+            $("#timer").html(game.timeAloted);
+            // // for (i = 0; i <= questions.length; i++) {
+            //     $("#dislplayQuestion").text(questions[i].question);
+            //     $("#answer_1").text(questions[i].answerChoices[0]);
+            //     $("#answer_2").text(questions[i].answerChoices[1]);
+            //     $("#answer_3").text(questions[i].answerChoices[2]);
+            //     $("#answer_4").text(questions[i].answerChoices[3]);
+            //     console.log(questions[i].answerChoices);
+            //     console.log(questions.length);
+            // // }
+            
+        },
+       
+        countDown: function() {
+            game.timeAloted--;
+            
+            if (game.timeAloted === 0) {
+                nextQuestion();
+                totalIncorrect++;
+                console.log("Times Up")
+            }
+        },
+
     }
+    // game.start();
+
+    
+
+    // Displays questions from object array
+
+    function nextQuestion() {
+        for (i = 0; i <= game.length; i++) {
+            $("#dislplayQuestion").text(game[i].question);
+            $("#answer_1").text(game[i].answerChoices[0]);
+            $("#answer_2").text(game[i].answerChoices[1]);
+            $("#answer_3").text(game[i].answerChoices[2]);
+            $("#answer_4").text(game[i].answerChoices[3]);
+            console.log(game[i].answerChoices);
+            console.log(game.length);
+        }
+    };
 
     // Checks to see if chosen answer is correct.
     function checkAnswer(answer, currentQuestion) {
@@ -66,7 +106,7 @@ $(document).ready(function () {
             totalIncorrect++
             // Alerts your choice was incorrect
             alert("This is the corrct answer" + currentQuestion.correct);
-            
+
             // Sets to next question
             i++
             // Displays next question
@@ -75,20 +115,22 @@ $(document).ready(function () {
     }
     // When click displays next question is anweres
     $("#startGame").on("click", function () {
-        nextQuestion();
+        game.start();
     });
 
     // Recieves answer chosen
     $(".answer").on("click", function () {
         var chosenAnswer = $(this).text();
-        checkAnswer(chosenAnswer, game[i])
+        checkAnswer(chosenAnswer, questions[i])
+        console.log(chosenAnswer);
 
     });
 
     // Checks to see if you've run out of questions
-    if (game[i] >= 3) {
-        alert("That's it. Thanks for playing");
-    }
+    // if (numberOfQuestionsAsked >= game.length) {
+    //     alert("That's it. Thanks for playing");
+
+    // }
 
     // Creat a count down function with setInterval
     // If timer reaches 0, display correct answer and tick up total incorrect.
